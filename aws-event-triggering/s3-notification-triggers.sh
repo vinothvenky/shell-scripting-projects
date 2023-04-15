4a75171abca5f9720b61460929a9cf745a0a6fa6
vinothvenky <98956763+vinothvenky@users.noreply.github.com>
2023-04-15 14:06:04 +0530

    Update s3-notification-triggers.sh

--- aws-event-triggering/s3-notification-triggers.sh
+++ aws-event-triggering/s3-notification-triggers.sh
@@ -10,10 +10,10 @@ echo "AWS Account ID: $aws_account_id"
 
 # Set AWS region and bucket name
 aws_region="us-east-1"
-bucket_name="abhishek-ultimate-bucket"
-lambda_func_name="s3-lambda-function"
+bucket_name="vinoth-scripting-bucket"
+lambda_func_name="s3-script-lambda-function"
 role_name="s3-lambda-sns"
-email_address="zyz@gmail.com"
+email_address="vinothvenky93.com"
 
 # Create IAM Role for the project
 role_response=$(aws iam create-role --role-name s3-lambda-sns --assume-role-policy-document '{
@@ -103,6 +103,6 @@ aws sns subscribe \
 aws sns publish \
   --topic-arn "$topic_arn" \
   --subject "A new object created in s3 bucket" \
-  --message "Hello from Abhishek.Veeramalla YouTube channel, Learn DevOps Zero to Hero for Free"
+  --message "Please subscribe to Abhishek.Veeramalla YouTube channel, Learn DevOps Zero to Hero for Free"
 
 
